d2f551358bcba3489cf42e5c890cbf1f7646a273
sujit <itsursujit@gmail.com>
2022-07-21 22:47:21 +0545

    update

--- src/components/ParentComponent.tsx
+++ src/components/ParentComponent.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 import { ChildComponent } from './ChildComponent'
 export const ParentComponent = () => {
   const nameList = ['Sujit', 'Anita', 'Alishma', 'Sanvi']
   const ClickHandler = (childName: string) => {
     alert(`Clicked! ${childName}`)
   }
+	const [username, setUsername] = useState('')
   return (
 		<div>
 			{nameList.map((name) => {
@@ -15,19 +17,19 @@ export const ParentComponent = () => {
 				)
 			})}
 			<div className="flex justify-center -space-x-3 font-mono text-white text-sm font-bold leading-6">
-				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 shadow-lg ring-2 ring-white z-40 dark:ring-slate-900">
+				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 hover:bg-pink-600 shadow-lg ring-2 ring-white z-40 dark:ring-slate-900">
 					05
 				</div>
-				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 shadow-lg ring-2 ring-white z-45 dark:ring-slate-900">
+				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 hover:bg-pink-600 shadow-lg ring-2 ring-white z-45 dark:ring-slate-900">
 					04
 				</div>
-				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 shadow-lg ring-2 ring-white z-20 dark:ring-slate-900">
+				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 hover:bg-pink-600 shadow-lg ring-2 ring-white z-20 dark:ring-slate-900">
 					03
 				</div>
-				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 shadow-lg ring-2 ring-white z-10 dark:ring-slate-900">
+				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 hover:bg-pink-600 shadow-lg ring-2 ring-white z-10 dark:ring-slate-900">
 					02
 				</div>
-				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 shadow-lg ring-2 ring-white z-0 dark:ring-slate-900">
+				<div className="w-16 h-16 rounded-full flex items-center justify-center bg-pink-500 hover:bg-pink-600 shadow-lg ring-2 ring-white z-0 dark:ring-slate-900">
 					01
 				</div>
 			</div>
